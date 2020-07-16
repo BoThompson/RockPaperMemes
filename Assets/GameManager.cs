4cae90cca71da4ad5bf1a167f2d55a4e812a47ce
Bo Thompson <AimlessZealot@gmail.com>
2020-07-16 14:17:10 -0400

    Added sounds and basic imagery

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -11,6 +11,15 @@ public class GameManager : MonoBehaviour
     public Canvas ResultsCanvas;
     public Canvas PlayAgainCanvas;
     public Canvas currentCanvas;
+    public AudioSource Victory;
+    public AudioSource Loss;
+    public Image P1Image;
+    public Image P2Image;
+    public Text ResultText;
+    public Sprite RockSprite;
+    public Sprite PaperSprite;
+    public Sprite MemeSprite;
+
     public enum Choice
     {
         Rock = 0,
@@ -93,9 +102,68 @@ public class GameManager : MonoBehaviour
         currentCanvas.gameObject.SetActive(true);
     }
 
+    public void GoToPlayAgain()
+    {
+        currentCanvas.gameObject.SetActive(false);
+        currentCanvas = PlayAgainCanvas;
+        currentCanvas.gameObject.SetActive(true);
+    }
+
     public void UpdateResults()
     {
+        switch(PlayerChoice)
+        {
+            case Choice.Rock: P1Image.color = Color.red; P1Image.sprite = RockSprite; break;
+            case Choice.Paper: P1Image.color = Color.blue; P1Image.sprite = PaperSprite; break;
+            case Choice.Meme: P1Image.color = (Color.red + Color.yellow) / 2; P1Image.sprite = MemeSprite; break;
+        }
 
+        switch (AIChoice)
+        {
+            case Choice.Rock: P2Image.color = Color.red; P2Image.sprite = RockSprite; break;
+            case Choice.Paper: P2Image.color = Color.blue; P2Image.sprite = PaperSprite; break;
+            case Choice.Meme: P2Image.color = (Color.red + Color.yellow) / 2; P2Image.sprite = MemeSprite; break;
+        }
+
+        if (PlayerChoice == Choice.Rock && AIChoice == Choice.Paper)
+        {
+            ResultText.text = "The AI just won!";
+            Victory.enabled = false;
+            Loss.enabled = true;
+        }else if (PlayerChoice == Choice.Rock && AIChoice == Choice.Meme)
+        {
+            ResultText.text = "You just won!";
+            Victory.enabled = true;
+            Loss.enabled = false;
+        }
+        else if (PlayerChoice == Choice.Paper && AIChoice == Choice.Meme)
+        {
+            ResultText.text = "The AI just won!";
+            Victory.enabled = false;
+            Loss.enabled = true;
+        }
+        else if (PlayerChoice == Choice.Paper && AIChoice == Choice.Rock)
+        {
+            ResultText.text = "You just won!";
+            Victory.enabled = true;
+            Loss.enabled = false;
+        }
+        else if (PlayerChoice == Choice.Meme && AIChoice == Choice.Rock)
+        {
+            ResultText.text = "The AI just won!";
+            Victory.enabled = false;
+            Loss.enabled = true;
+        }
+        else if (PlayerChoice == Choice.Meme && AIChoice == Choice.Paper)
+        {
+            ResultText.text = "You just won!";
+            Victory.enabled = true;
+            Loss.enabled = false;
+        }
+        else
+        {
+            ResultText.text = "Nobody won!";
+        }
     }
     // Update is called once per frame
     void Update()
